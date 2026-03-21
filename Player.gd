de7289b0c4350ef502209e5da4a640bee6291586
Aryan Tadepalli <79216214+g5works@users.noreply.github.com>
2026-03-21 15:22:36 -0400

    add some crosshair stuff

--- Player.gd
+++ Player.gd
@@ -1,25 +1,29 @@
 extends RigidBody2D
 
-@onready var line := get_owner().get_node("indicator") as Line2D
-@onready var striker := get_owner().get_node("striker") as RayCast2D;
+@onready var line := get_parent().get_node("indicator") as Line2D
+@onready var striker := get_parent().get_node("striker") as RayCast2D;
+@onready var sprite := get_node("PlayerSprite") as Sprite2D;
+@onready var linestart := sprite.get_node("Grab Point") as Node2D;
 var button_force = Vector2(0,0);
 var physics_force = Vector2(0,0);
 var forcable = false
+var castresult: Object = null;
 
 func _init():
 	pass
 
 func _process(delta):
-		
+	
+
 	var mouse_position = line.get_local_mouse_position();
 	
-	line.points[0] = global_position;
-	line.points[1] = line.get_local_mouse_position();
+	line.points[0] = linestart.global_position;
 
 	
 	striker.position = global_position;
 	striker.target_position = striker.get_local_mouse_position();
 	striker.add_exception(self)
+	
 	
 	if forcable: 
 		line.default_color = Color(0, 1, 1, 0.25);
@@ -36,30 +40,39 @@ func _process(delta):
 		button_force.x = 30000;
 	else:
 		button_force = Vector2(0,0);
+		
+	if castresult != null:
+		line.points[1] = castresult.global_position;
+
+	else:
+		line.points[1] = line.get_local_mouse_position();
+
+	#
+	#var dir2mouse: Vector2 = global_position.direction_to(get_local_mouse_position())
+	#var perp_dir2mouse: Vector2 = Vector2(-dir2mouse.y, dir2mouse.x);
+	#var t: Transform2D = Transform2D();
+	#t.y = dir2mouse;
+	#t.x = perp_dir2mouse;
+	
+	
 
 func _physics_process(delta):
 	
 	apply_force(physics_force+button_force, Vector2(0,0));
 	
-	var result: Object = striker.get_collider();	
+	castresult = striker.get_collider();	
 	
-	if result != null:
+	if castresult != null:
 		forcable = true;
-		var direction: Vector2 = global_position.direction_to(result.global_position);
+		var direction: Vector2 = global_position.direction_to(castresult.global_position);
 		if Input.is_action_pressed("Pull"):
 			physics_force = direction*10000;
-			result.apply_force(-direction*10000);
+			castresult.apply_force(-direction*10000);
 		else: 
 			physics_force = Vector2(0,0);
 		
 	else:
 		forcable = false;
-		
 
-		
-		
-	print(result)
-	
-	
 func _input(event):
 	pass
